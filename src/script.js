3955e9b990ce798b4c35291bb100adede575ff93
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-03-03 14:42:17 +0530

    update fetch link from local to github

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function fetchScores() {
-    fetch('data/scores.json') // Replace with the actual URL of your JSON file
+    fetch('https://raw.githubusercontent.com/Quantaindew/hot-reload-without-backend/main/data/scores.json') // Replace with the actual URL of your JSON file
         .then(response => response.json())
         .then(data => {
             document.getElementById('teamAScore').textContent = data.teamA;
