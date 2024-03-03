c6ce1c96519ae40082ad96528bd2b8059c2402d4
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-03-03 14:33:35 +0530

    fixed scores file path

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function fetchScores() {
-    fetch('../data/scores.json') // Replace with the actual URL of your JSON file
+    fetch('/data/scores.json') // Replace with the actual URL of your JSON file
         .then(response => response.json())
         .then(data => {
             document.getElementById('teamAScore').textContent = data.teamA;
